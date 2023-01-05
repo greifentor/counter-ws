3a5fa0e1b0ef0fa8b496e722fe9d91a842c13072
ollie <lieshoff@gmx.de>
2023-01-05 20:44:25 +0100

    Some changed while working on the code generators.

--- gui-web/src/main/java/de/ollie/counter/ws/gui/AccessChecker.java
+++ gui-web/src/main/java/de/ollie/counter/ws/gui/AccessChecker.java
@@ -1,5 +1,13 @@
 package de.ollie.counter.ws.gui;
 
+import lombok.Generated;
+
+/**
+ * An interface for access checkers.
+ *
+ * GENERATED CODE !!! DO NOT CHANGE !!!
+ */
+@Generated
 public interface AccessChecker {
 
 	boolean isSessionValid();
