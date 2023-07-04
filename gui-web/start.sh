0f3a27daba7808f06edfe42fc981cbff1e8104e9
ollie <lieshoff@gmx.de>
2023-07-04 23:32:48 +0200

    Added additional display information and a standalone mode for testing.

--- gui-web/start.sh
+++ gui-web/start.sh
@@ -1,1 +1,1 @@
-java -Dserver.port=8082 -Dcube.url=http://localhost:8080/cube -jar target/counter-ws-gui-1.7.0.jar
+java -Dserver.port=8082 -Dcube.disabled.user.name=OTFLZT-0000000001 -Dcube.enabled=false -Dcube.url=http://localhost:8080/cube -jar target/counter-ws-gui-1.7.1.jar
